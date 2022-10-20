c6a54eed46fd0bbd93f3bcba6c0f568e73dd986c
Nish-026 <nishtha.kashyap2608@gmail.com>
2022-10-20 22:15:59 +0530

    added 2 codes

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+var str="naman";
+var rstr="";
+
+for(i=0;i<str.length;i++)
+{
+   rstr=str[i]+rstr;
+}
+
+if(rstr!=str)
+{
+console.log("Yes")
+}
+else
+{
+console.log("No")
+}
+
---
+++ prime_number.js
@@ -0,0 +1,18 @@
+var num=13;
+var count=0;
+for(i=1;i<=num;i++)
+{
+if(num%i==0)
+{
+ count++
+}
+
+}
+if(count==0)
+{
+  console.log("yes")
+}
+else
+{
+console.log("No")
+}
